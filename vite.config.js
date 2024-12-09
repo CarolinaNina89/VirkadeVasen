717d528518fac22ff8376c2d58ff05c15cb2375a
Carolina Jakobsson <carolina.jakobsson@hiq.se>
2024-12-09 14:57:53 +0100

    adding dist/ folder

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   server: {
-//     open: true, // Öppna automatiskt webbläsaren vid uppstart
-//   },
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       // Specifik upplösning för React
-//       react: "react",
-//       "react-dom": "react-dom",
-//     },
-//   },
-//   optimizeDeps: {
-//     include: ["react", "react-dom"], // Explicitly include react and react-dom for dependency optimization
-//   },
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/VirkadeVasen/",
   plugins: [react()],
 });
